98c1a6586c970ab94f73f3e3fd650877869e68d2
Red Cat <alexandra.andreeva.korbit@gmail.com>
2018-11-29 18:16:28 +0200

    add router

---
+++ src/app/router/router.module.spec.ts
@@ -0,0 +1,14 @@
+import { RouterModule } from './router.module';
+
+
+describe('RouterModule', () => {
+  let routerModule: RouterModule;
+
+  beforeEach(() => {
+    routerModule = new RouterModule();
+  });
+
+  it('should create an instance', () => {
+    expect(routerModule).toBeTruthy();
+  });
+});
