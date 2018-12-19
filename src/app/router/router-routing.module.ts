cf68a056ec26e18c4e4b5ea7b8adb5c349cd7f2f
Artur Belogur <artur.belogur.korbit@gmail.com>
2018-12-20 01:21:43 +0200

    Fix left menu refreshing

--- src/app/router/router-routing.module.ts
+++ src/app/router/router-routing.module.ts
@@ -1,20 +1,40 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {EquipmentComponent} from '../components/equipment/equipment.component';
 import {ReportComponent} from '../components/report/report.component';
 import {DefectComponent} from '../components/defect/defect.component';
+import {EquipmentDashboardComponent} from '../components/equipment/equipment-dashboard/equipment-dashboard.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'equipment', pathMatch: 'full' },
-  { path: 'report', component: ReportComponent  },
-  { path: 'defects', component: DefectComponent  },
-  { path: 'equipment', component: EquipmentComponent  },
-  { path: 'equipment/:id', component: EquipmentComponent }
+  {
+    path: '',
+    redirectTo: 'equipment',
+    pathMatch: 'full'},
+  {
+    path: 'report',
+    component: ReportComponent
+  },
+  {
+    path: 'defects',
+    component: DefectComponent},
+  {
+    path: 'equipment',
+    component: EquipmentComponent,
+    children: [
+      {
+        path: '', component: EquipmentDashboardComponent
+      },
+      {
+        path: ':id', component: EquipmentDashboardComponent
+      }
+    ]
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class RouterRoutingModule { }
+export class RouterRoutingModule {
+}
